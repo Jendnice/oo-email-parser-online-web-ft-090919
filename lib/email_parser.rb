fcfa74f902f28578f7953eab1f236561978e6d38
Jennifer Nicely <pipeline38@hotmail.com>
2019-09-20 22:39:13 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -8,12 +8,6 @@ class EmailAddressParser
   
   def initialize(emails)
     @emails = emails 
-    # def parse 
-    #   array = @emails.split(",")
-    #   new_string = array.join(" ")
-    #   list = new_string.split(" ")
-    #   list.uniq 
-    # end 
   end 
   
   def parse 
@@ -22,4 +16,6 @@ class EmailAddressParser
       list = new_string.split(" ")
       list.uniq 
     end 
+    
+    @emails.split(",").join(" ").split(" ").uniq 
 end 
